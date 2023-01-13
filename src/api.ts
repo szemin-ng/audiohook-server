f3a004b80dc6866fe4b3c8fa89c1d61b33ce7b69
szemin-ng <szemin.ng@gmail.com>
2023-01-13 13:30:20 +0800

    Added ability to serve WAV files

---
+++ src/api.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import path from "path";
+import * as fsPromises from "fs/promises";
+import logger from "./log";
+import { environment } from "./environment";
+
+type AudioFile = {
+  id: string;
+  date: Date;
+  url: string;
+}
+
+export async function listAudioFiles(req: Request, res: Response, next: NextFunction) {
+  try {
+    // get list of .wav files
+    let files = (await fsPromises.readdir(path.join(__dirname, environment.mediaPath))).filter(f => f.endsWith(".wav"));
+
+    // create AudioFile[]
+    // Intentionally Using a for loop here
+    let audioFiles: AudioFile[] = [];
+    for (let i = 0; i < files.length; i++) {
+      let modifiedDate = (await fsPromises.stat(path.join(__dirname, environment.mediaPath, files[i]))).mtime;
+      let audioFile: AudioFile = {
+        id: files[i],
+        date: modifiedDate,
+        url: `/media/${files[i]}`
+      }
+      audioFiles.push(audioFile);
+    }
+
+    res.status(StatusCodes.OK).json(audioFiles);
+
+    if (next) next();
+  } catch (err) {
+    next(err);
+  }
+}
